a4b9c14cf282abe48fe55ead2c2349ea5a9d3bee
hanfengs <yifan1223@gmail.com>
2017-11-24 11:03:10 +0800

    Initial Commit

---
+++ HFSpriteKitDemo/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  HFSpriteKitDemo
+//
+//  Created by hanfeng on 2017/11/24.
+//  Copyright © 2017年 hanfeng. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ HFSpriteKitDemo/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  HFSpriteKitDemo
+//
+//  Created by hanfeng on 2017/11/24.
+//  Copyright © 2017年 hanfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
